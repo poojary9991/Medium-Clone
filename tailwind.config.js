317bb0f9ba080262140ed86286271cd17fb0fbf1
poojary9991 <poojaryshipra14@gmail.com>
2024-10-15 01:17:18 -0400

    added the files and changes layout.tsx and page.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
