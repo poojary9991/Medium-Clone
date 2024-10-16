ed11d26f16f82c45964995e2ea57e9eec3a62f3f
poojary9991 <poojaryshipra14@gmail.com>
2024-10-16 11:29:27 -0400

    added the css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 import Link from "next/link";
 
-
-
 export default async function Home() {
   const query = `*[_type == "post"]{
     _id,
@@ -21,6 +19,7 @@ export default async function Home() {
   }`;
   
   const posts: Post[] = await sanityClient.fetch(query);
+
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -37,13 +36,22 @@ export default async function Home() {
         </div>
         <img className="hidden md:inline-flex h-32 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
       </div>
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map(post =>(
           <Link key={post._id} href={'/post/${post.slug.current}'}>
-            <div>
-              <img src={
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img className="h-60 w-full object-cover group-hover:scale-105 transition-reansform duration-200 ease-in-out" src={
                 urlFor(post.mainImage).url()!
               } alt="" />
+              <div className="flex justify-between p-5 bg-white text-black">
+                <div>
+                <p className="text-lg font-bold">{post.title}</p>
+                <p className="text-xs">
+                  {post.description} by {post.author.name}
+                </p>
+                </div>
+                <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+              </div>
             </div>
           </Link>
         ))}
